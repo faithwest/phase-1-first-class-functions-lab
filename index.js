5492fd3120e747c3eb18811c760ca695d4307cd3
faithwest <faithchepkoech425@gmail.com>
2023-09-22 18:39:37 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(driversArray) {
+    return driversArray.slice(0, 2);
+  }
+
+  function returnLastTwoDrivers(driversArray) {
+    return driversArray.slice(-2);
+  }
+
+  let selectingDrivers = [
+     returnFirstTwoDrivers, returnLastTwoDrivers  
+  ]
+   function createFareMultiplier(multiplier)  {
+     return function(fare) {
+    return fare * multiplier
+   }
+}
+    
+    function fareDoubler(fare) {
+          return fare * 2;
+        };
+      
+    function fareTripler(fare) {
+        return fare * 3
+    };
+  
+    function selectDifferentDrivers(arrayOfDrivers, Function) {
+        return Function(arrayOfDrivers);
+      }
+
+
+
+  // Code your solution in this file!
